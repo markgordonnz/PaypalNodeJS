a323cfb428bf4cdce099a9ffdc928b551262ecd9
markgordonnz <44230575+markgordonnz@users.noreply.github.com>
2018-11-27 23:42:46 +1100

    First Commit

---
+++ views/includes/js/paypal.js
@@ -0,0 +1,26 @@
+// Get the js assigned to this pricing
+var modal = document.getElementById('initialjs');
+
+// Get the button that opens the popup
+var btn = document.getElementById("initial");
+
+// Get the button to cancel the transaction
+var btnclose = document.getElementsByClassName("cancelbutton")[0];
+
+// When the user clicks on the button, call the initjs
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on cancel close popup
+btnclose.onclick = function() {
+    modal.style.display = "none";
+    window.location.hash = '#pricing';
+}
+
+// When the user clicks anywhere outside of the popup close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
